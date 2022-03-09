cce8fab4423cdd87070717626db68b270b9fcc3d
Sahil Aujla <sahilaujla15@gmail.com>
2022-03-09 06:55:19 +0530

    Updated the deploy.js script to save ABI and Contract Address to the frontend.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,7 @@
 const { ethers } = require("hardhat");
 
+let contractAddress;
+
 async function main() {
   /*
   A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
@@ -16,6 +18,42 @@ async function main() {
 
   // print the address of the deployed contract
   console.log("Whitelist Contract Address:", deployedWhitelistContract.address);
+
+  contractAddress = deployedWhitelistContract.address;
+
+  saveAbi();
+  saveContractAddress();
+}
+
+function saveAbi() {
+  const fs = require("fs");
+
+  const abiDir = __dirname + "/../../dapp/constants";
+
+  if (!fs.existsSync(abiDir)) {
+    fs.mkdirSync(abiDir);
+  }
+
+  const artifact = artifacts.readArtifactSync("Whitelist");
+
+  fs.writeFileSync(
+    abiDir + "/Whitelist.json",
+    JSON.stringify(artifact, null, 2)
+  );
+}
+
+function saveContractAddress() {
+  const fs = require("fs");
+
+  const abiDir = __dirname + "/../../dapp/constants";
+
+  if (!fs.existsSync(abiDir)) {
+    fs.mkdirSync(abiDir);
+  }
+
+  const data = `export const WHITELIST_CONTRACT_ADDRESS = "${contractAddress}"`;
+
+  fs.writeFileSync(abiDir + "/contract.js", data);
 }
 
 // Call the main function and catch if there is any error
